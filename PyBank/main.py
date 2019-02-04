1ad0606ca6bef492542911e1c21e473c9a2cf792
mardakis26 <mardakis26@gmail.com>
2019-02-04 09:45:08 -0500

    Semifinal product, missing text file creation

--- PyBank/main.py
+++ PyBank/main.py
@@ -32,16 +32,38 @@ PL_sum = sum(profitloss)
 
 #get average of the changes each period
 profitchange = []
-for x in profitloss:    
-    x = 1
-    profitchange  = ((profitloss[x] - profitloss[x-1]))/total_months
-    
-    
+for x in range(1,(len(months))):    
+    profitchange.append((profitloss[x] - profitloss[x-1]))
+    #gives average change in profit/loss
+    avg_chg = sum(profitchange) / len(profitchange)
 
-print(profitchange)
+#need to find max amount and corresponding date
 
+#shows max profit
+maxprofit = max(profitchange)
+#gives index of max profit so I can match it to the date in column 0
+max_index = profitchange.index(maxprofit) + 1
+#shows date that corresponds to max profit
+maxprofit_date = months[max_index]
 
-        
+#shows min profit
+minprofit = min(profitchange)
+#gives index of min profit so I can match it to the date in column 0
+min_index = profitchange.index(minprofit) + 1
+#shows date that corresponds to min profit
+minprofit_date = months[min_index]
+
+#final results
+print("Financial Analysis")
+print("-----------------------")
+print("Total Months: " + str(total_months))
+print("Total: " + str(PL_sum))
+print("Average Change: " + str(avg_chg))
+print("Greatest Increase in Profits: " + str(maxprofit) + " (" + str(maxprofit_date) + ")")
+print("Greatest Decrease in Profits: " + str(minprofit) + " (" + str(minprofit_date) + ")")
+
+#export results to text file
+
 
 
 
